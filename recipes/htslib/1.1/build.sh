6bf8ae19bc95b1a2a757ef7844b0f294936ec048
John Greeley <jgreeley@bostonlighthouse.us>
2019-12-13 20:41:50 -0500

    removing configure

--- recipes/htslib/1.1/build.sh
+++ recipes/htslib/1.1/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX --enable-libcurl CFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
-make install prefix=$PREFIX CC=$CC
+make prefix=$PREFIX CC=$CC install
