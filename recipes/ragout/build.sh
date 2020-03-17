3b6f702f8360af65f12192a5ad4bde322768a0a7
Mikhail Kolmogorov <fenderglass@gmail.com>
2020-03-16 20:33:25 -0700

    try 4

--- recipes/ragout/build.sh
+++ recipes/ragout/build.sh
@@ -2,8 +2,5 @@
 # builds binary modules: ragout-maf2synteny ragout-overlap
 python setup.py build
 
-#runs tests
-$PYTHON scripts/run-tests.py
-
 #installs ragout as a python pachage
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
