2713aec2ebe754e63380d616c881dd3c4255982a
Andrea Telatin <andrea.telatin@quadram.ac.uk>
2021-02-05 17:29:00 +0000

    [seqfu] Update dependencies for future release

--- recipes/seqfu/build.sh
+++ recipes/seqfu/build.sh
@@ -8,13 +8,20 @@ fi
 
 mkdir -p $PREFIX/bin
 
-nimble install -y --verbose argparse docopt terminaltables
+# Dependencies
+nimble install -y --verbose argparse docopt terminaltables iterutils readfq
+
+# Build program
 nim c --threads:on -p:lib --opt:speed -d:release -o:$PREFIX/bin/seqfu src/sfu.nim
 
-
+# Accessory utils
 for SOURCE in src/fu_*.nim;
 do
 	NAME=$(basename $SOURCE | sed 's/_/-/'  |cut -f1 -d.)
+	THREADS=""
+	if [[ $(grep thread "$UTIL")  ]]; then
+  	 THREADS=" --threads:on "
+ 	fi
 	echo "$SOURCE -> $NAME"
-	nim c -p:lib --opt:speed -d:release -o:"$PREFIX/bin/${NAME}" "$SOURCE" || true
+	nim c $THREADS -p:lib --opt:speed -d:release -o:"$PREFIX/bin/${NAME}" "$SOURCE" || true
 done
